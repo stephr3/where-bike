7780f8005627f84b2deebe6d44c77f2dbb6d8be2
Brian Grant and Stephanie Gurung <brian@>
2016-08-03 16:50:37 -0700

    end of day

--- js/bike.js
+++ js/bike.js
@@ -1,13 +1,34 @@
 var bikeKey = require('./../.env').bikeKey;
 
-function Bike() {}
+function Bike() {
+  this.lattLong = [];
+}
+var locations = [];
+var lattLong = [];
 
 Bike.prototype.getBikeList = function(city){
   $.get("https://bikeindex.org:443/api/v2/bikes_search/stolen?per_page=100&proximity="+ city +"&proximity_square=2").then(function(response){
     console.log(response);
+    response.bikes.forEach(function(bike){
+      locations.push(bike.stolen_location);
+    });
+    console.log(locations);
+  }).then(function(response){
+    locations.forEach(function(place){
+    $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?address="+encodeURIComponent(place), function(val) {
+      if(val.results.length) {
+        var location = val.results[0].geometry.location;
+        var lat = location.lat;
+        var lon = location.lng;
+        // $("#lat-long-array").append("[" + lat + ", " + lon + "], ");
+        lattLong.push([lat, lon]);
+      }
+      });
+    }
+    // console.log(lattLong);
   }).fail(function(error){
     $("#error").text(error.responseJSON.message);
-  })
+  });
 };
 
 exports.bikeModule = Bike;
--- js/city-interface.js
+++ js/city-interface.js
@@ -6,14 +6,6 @@ $(document).ready(function(){
     event.preventDefault();
     var city = $("#city").val();
     $("#city").val("");
-    $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?address="+encodeURIComponent(city), function(val) {
-      if(val.results.length) {
-        var location = val.results[0].geometry.location
-        var lat = location.lat;
-        var lon = location.lng;
-        console.log(city + ":" + lat + ", " + lon)
-      }
-    })
     var newBikeSearch = new Bike();
     newBikeSearch.getBikeList(city);
   });
