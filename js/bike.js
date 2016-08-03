e02170612e726b716fd5bbabcd6c7814e9cdff23
Brian Grant and Stephanie Gurung <brian+stephanie@>
2016-08-03 14:51:36 -0700

    log most recent 100 bikes stolen in user inputted city

--- js/bike.js
+++ js/bike.js
@@ -2,8 +2,8 @@ var bikeKey = require('./../.env').bikeKey;
 
 function Bike() {}
 
-Bike.prototype.getBikeList = function(color){
-  $.get("https://bikeindex.org:443/api/v2/bikes_search/stolen?per_page=200&proximity=45.5207086%2C-122.6795858&proximity_square=1").then(function(response){
+Bike.prototype.getBikeList = function(city){
+  $.get("https://bikeindex.org:443/api/v2/bikes_search/stolen?per_page=100&proximity="+ city +"&proximity_square=2").then(function(response){
     console.log(response);
   }).fail(function(error){
     $("#error").text(error.responseJSON.message);
--- js/city-interface.js
+++ js/city-interface.js
@@ -1,11 +1,13 @@
 var mapsKey = require('./../.env').mapsKey;
+var Bike = require('./../js/bike.js').bikeModule;
 
 $(document).ready(function(){
   $("#city-search").click(function(event){
-    alert('clicked');
     event.preventDefault();
     var city = $("#city").val();
     $("#city").val("");
     $("iframe").attr("src", "https://www.google.com/maps/embed/v1/place?q=" + city + "&key=" + mapsKey)
+    var newBikeSearch = new Bike();
+    newBikeSearch.getBikeList(city);
   });
 });
